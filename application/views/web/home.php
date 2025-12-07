fbad6565693f8ee7950e1890160ed33acd66c072
Muhammad Ulil Albab <ulilalbab77u@gmail.com>
2025-12-07 16:18:23 +0700

    feat: update website branding and improve UI sections

--- application/views/web/home.php
+++ application/views/web/home.php
@@ -331,61 +331,7 @@
     </div>
     <!-- Restaurant End -->
 
-
-    <!-- Service Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">What We Do</div>
-                <h1 class="display-6 mb-5">Learn More What We Do And Get Involved</h1>
-            </div>
-            <div class="row g-4 justify-content-center">
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                        <img class="img-fluid mb-4" src="<?=base_url()?>assets/web/img/icon-1.png" alt="">
-                        <h4 class="mb-3">Child Education</h4>
-                        <p class="mb-4">Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed vero dolor duo.</p>
-                        <a class="btn btn-outline-primary px-3" href="">
-                            Learn More
-                            <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                <i class="fa fa-arrow-right"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                        <img class="img-fluid mb-4" src="<?=base_url()?>assets/web/img/icon-2.png" alt="">
-                        <h4 class="mb-3">Medical Treatment</h4>
-                        <p class="mb-4">Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed vero dolor duo.</p>
-                        <a class="btn btn-outline-primary px-3" href="">
-                            Learn More
-                            <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                <i class="fa fa-arrow-right"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                        <img class="img-fluid mb-4" src="<?=base_url()?>assets/web/img/icon-3.png" alt="">
-                        <h4 class="mb-3">Pure Drinking Water</h4>
-                        <p class="mb-4">Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed vero dolor duo.</p>
-                        <a class="btn btn-outline-primary px-3" href="">
-                            Learn More
-                            <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                <i class="fa fa-arrow-right"></i>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Service End -->
-
-
-    <!-- Donate Start -->
+    <!-- Preview Start -->
     <div class="container-fluid donate my-5 py-5" data-parallax="scroll" data-image-src="<?=base_url()?>assets/web/img/carousel-2.jpg">
         <div class="container py-5">
             <div class="row g-5 align-items-center">
@@ -452,23 +398,7 @@
             </div>
         </div>
     </div>
-    <!-- Donate End -->
-
-    <!-- Maps Start -->
-    <div id="maps" class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Nearest Restaurant</div>
-                <h1 class="display-6 mb-5">Find your favorite restaurant</h1>
-            </div>
-            <div class="row g-4 justify-content-center">
-                <div class="col-12">
-                    <iframe src="<?=base_url()?>/home" width="100%" height="900" style="border: none; min-height: 600px; max-height: 90vh; border-radius: 8px;"></iframe>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Maps End -->
+    <!-- Preview End -->
 
     <!-- Team Start -->
     <div class="container-xxl py-5">
